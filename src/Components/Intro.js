ab594ac1f3f93ea0812f753538b8f07deb1e2f4d
David Fertitta <Davidj.fertitta@gmail.com>
2022-10-17 15:16:53 -0600

    blurb update

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -61,9 +61,11 @@ const Intro = () => {
             </div>
             <p className='blurb bottom'>
                 <b className='bold-blurb'>NOTE: </b>To activate the links in
-                this menu, click on a resource in the Student eBook link for
-                Unit 1. This will ensure that you have an active session on Ed
-                before you begin clicking on Grammar Resources.
+                this menu, go to Discover and click on a resource in the Student
+                eBook for Unit 1. This will ensure that you have an active
+                session on Ed before you begin clicking on Grammar Resources. If
+                you do not have an active session, you will be redirected to the
+                HMH home page.
             </p>
         </div>
     );
